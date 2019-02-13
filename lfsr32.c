18170358e3fad225abd997385a1ff7b991d39140
Francesco Conti <f.conti@unibo.it>
2019-02-13 17:52:38 +0100

    Added license (MIT)

--- lfsr32.c
+++ lfsr32.c
@@ -1,6 +1,12 @@
 /*
+ * Copyright 2019 Francesco Conti <francesco.conti88@gmail.com>
+ *
+ * Adapted from https://github.com/russm/lfsr64
  * This is a simple 32-bit linear feedback shift register, printing
  * pseudo-random bytes to stdout.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
  */
 
 #include <stdint.h>
